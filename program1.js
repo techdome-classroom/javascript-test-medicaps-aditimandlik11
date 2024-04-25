cfbd2ffcd635a3dd53e34201e470de3004d46b62
aditimandlik11 <168070054+aditimandlik11@users.noreply.github.com>
2024-04-25 14:14:28 +0530

    Add files via upload

--- program1.js
+++ program1.js
@@ -1,6 +1,29 @@
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
-}
+
+        if (!s) {
+            return 0;
+        }
+        let start = 0;
+
+        let end = 0;
+        
+        let maxLength = 0;
+        
+        const uniqueCharacters = new Set();
+        
+        while (end < s.length) {
+        
+            if (!uniqueCharacters.has(s[end])) {
+                uniqueCharacters.add(s[end]);
+                end++;
+                maxLength = Math.max(maxLength, uniqueCharacters.size);
+            } else {
+                uniqueCharacters.delete(s[start]);
+                start++;
+            }
+        }
+        return maxLength;
+    };
 
 module.exports = { longestSubstring };
 
--- program2.js
+++ program2.js
@@ -1,6 +1,33 @@
 function smallestMissingPositiveInteger(nums) {
 
-  // Implement the function smallest_missing_positive_integer
+  let n = nums.length;
+        
+        let present = new Array(n+1);
+        
+        
+        for(let i=0;i<n+1;i++)
+        {
+            present[i]=false;
+        }
+
+        for (let i = 0; i < n; i++)
+        {
+            
+            if (nums[i] > 0 && nums[i] <= n)
+            {
+                present[nums[i]] = true;
+            }
+        }
+
+        for (let i = 1; i <= n; i++)
+        {
+            if (!present[i])
+            {
+                return i;
+            }
+        }
+        
+        return n + 1;
 
 }
 
